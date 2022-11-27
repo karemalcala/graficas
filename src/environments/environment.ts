14b829d441dba1d298e1d3c156399aec62f9e167
Karem Alcala <kalcala@buk.cl>
2022-11-27 09:02:10 -0300

    init: config and delete test file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyArGiRgGd2MfE65_9sjE2QX49gt1sP0GmA",
+    authDomain: "racional-exam.firebaseapp.com",
+    databaseURL: "https://racional-exam.firebaseio.com",
+    projectId: "racional-exam",
+    storageBucket: "racional-exam.appspot.com",
+    messagingSenderId: "669314004725",
+    appId: "1:669314004725:web:48bd14a97d7db43c91f7bc"
+  },
   production: false
 };
 
